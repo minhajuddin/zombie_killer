fd327ee9569842cec437ac8ea54d3c558b3064b1
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2018-04-12 16:12:28 +0530

    save

--- lib/mix/tasks/zombie_killer/scan.ex
+++ lib/mix/tasks/zombie_killer/scan.ex
@@ -8,19 +8,18 @@ defmodule Mix.Tasks.ZombieKiller.Scan do
   def run([otp_app]) do
     otp_app = String.to_atom(otp_app)
     Mix.shell().info("Finding dead code...")
-    #:ok = Application.ensure_started(otp_app)
 
     IO.puts("# Potential dead code")
     IO.puts("=====================")
 
-    otp_app
-      |> IO.inspect(label: ">>>>>>>>>>>>>>>>")
-    |> get_mod_deps
-    |> Enum.filter(fn {_, deps_count} -> deps_count == 0 end)
-    |> Enum.map(fn {mod, _} -> mod end)
-    |> Enum.sort()
-    |> Enum.join("\n")
-    |> IO.puts()
+    get_files
+    |> IO.inspect(label: ">>>>>>>>>>>>>>>>")
+
+    # |> Enum.filter(fn {_, deps_count} -> deps_count == 0 end)
+    # |> Enum.map(fn {mod, _} -> mod end)
+    # |> Enum.sort()
+    # |> Enum.join("\n")
+    # |> IO.puts()
   end
 
   def run(_) do
@@ -28,26 +27,21 @@ defmodule Mix.Tasks.ZombieKiller.Scan do
     Invalid args.
 
     Usage:
-      mix zombie_killer:scan your_otp_app
+      mix do app.start, zombie_killer:scan your_otp_app
 
     Example:
-      mix zombie_killer:scan logger
+      mix do app.start, zombie_killer:scan logger
     """)
   end
 
-  def get_mod_deps(otp_app) do
-    with {:ok, modules} <- :application.get_key(otp_app, :modules) do
-      for mod <- modules do
-        module_wo_prefix = mod |> Module.split() |> Enum.join(".")
+  def get_files do
+    mix_run(:xref, ~w[graph --only-nodes])
+  end
 
-        deps_count =
-          capture_io(fn ->
-            Mix.Task.run(:xref, ["callers", module_wo_prefix])
-          end)
-          |> String.length()
-
-        {module_wo_prefix, deps_count}
-      end
-    end
+  def mix_run(task, args) do
+    capture_io(fn ->
+      Mix.Task.run(task, args)
+    end)
+    |> String.split("\n", trim: true)
   end
 end
